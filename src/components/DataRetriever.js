7abde24e3c8a7699543dc980ed742a8023a0ad46
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-09-19 18:07:38 -0400

    update

--- src/components/DataRetriever.js
+++ src/components/DataRetriever.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import brain from "brain.js";
+// import brain from "brain.js";
 
 class DataRetriever extends Component {
   constructor(props) {
