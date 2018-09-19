344c66e2c9f6821a7052271ef476cb2d23a1bb30
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-09-19 12:06:37 -0400

    componenet setpup

---
+++ src/components/NeuralNetwork.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import DataRetriever from "DataRetriever";
+
+class NeuralNetwork extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+}
+export default NeuralNetwork;
